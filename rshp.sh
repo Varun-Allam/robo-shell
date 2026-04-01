7fe93e062019c1b2861b4b44bd10a6ec9c535cad
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-04-01 20:00:44 +0200

    Update rshp.sh

--- rshp.sh
+++ rshp.sh
@@ -3,22 +3,35 @@
 AMI_ID="ami-0220d79f3f480ecf5" 
 SG_ID="sg-0e2dffc85a2dd9b4b" 
 
-for instance in $@ #$@ is taking dynamically 
+for instance in "$@"   # take dynamic inputs
 do 
-    INSTANCE_ID=$( aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0e2dffc85a2dd9b4b --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text ) 
+    echo "Creating instance: $instance"
 
-    if [ $instance != "frontend" ]; then
-        IP=$( aws ec2 describe-instances \
-  --instance-ids i-029ca58cbd2792c2b \
-  --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-  --output text ) 
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type t3.micro \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
+    echo "Instance ID: $INSTANCE_ID"
+
+    # wait until instance is running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
     else 
-        IP=$( aws ec2 describe-instances \
-  --instance-ids i-029ca58cbd2792c2b \
-  --query 'Reservations[0].Instances[0].PublicIpAddress' \
-  --output text )
+        IP=$(aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
     fi 
 
     echo "$instance: $IP" 
+    echo "-----------------------------"
 done
