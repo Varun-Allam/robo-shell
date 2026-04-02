8907c55899c5f77f8efdcee785cac6f9bb9b97fc
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-04-02 14:45:09 +0200

    Update rshp.sh

--- rshp.sh
+++ rshp.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5" 
 SG_ID="sg-0e2dffc85a2dd9b4b" 
+DOMAIN_NAME="sandhya.fun" 
+HOST_ZONE_ID="Z07101602FRRRZHC331MP"
 
 
 for instance in "$@"   # take dynamic inputs
@@ -26,13 +28,32 @@ do
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" 
     else 
         IP=$(aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi 
 
     echo "$instance: $IP" 
     #echo "-----------------------------"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOST_ZONE_ID \
+    --change-batch '{
+        "Changes": [{
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+            "Name": "'$RECORD_NAME'",
+            "Type": "A",
+            "TTL": 1,
+            "ResourceRecords": [{"Value": "'$IP'"}]
+        }
+        }]
+    }'
+
+
+
 done
